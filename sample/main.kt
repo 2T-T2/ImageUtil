21cc3018ab7adec92e880244b159075ea496cb4b
2T-T2 <70562172+2T-T2@users.noreply.github.com>
2021-09-14 04:56:34 +0900

    Create main.kt

---
+++ sample/main.kt
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.awt.*;
+
+fun main( args: Array<String> ) {
+    if ( args.size == 0 ) { println("引数を指定してください"); return; }
+    val f = File( args[0] );
+    if ( !ImageUtil.isImageFile(f) ) { println( "ファイルが存在しない、もしくは画像ファイルではありません" ); return; }
+
+    val imgUtil = ImageUtil(f);
+    imgUtil.addBorderText( "Hello World !", Font(Font.SERIF, Font.PLAIN, 30), Color.white, Color.black, 30, 30, 2 );
+    imgUtil.save( /* 出力ファイルパス */ );
+}
